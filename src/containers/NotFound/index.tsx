3caff40ba9c01ee05a8b85d7688622bd40fa8721
kevin <goldapple820@gmail.com>
2022-03-26 02:12:28 -0400

    added: NotFound component

--- src/containers/NotFound/index.tsx
+++ src/containers/NotFound/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NotFound() {
-  return <div>Page Not Found</div>;
+  return (
+    <div className="h-100 text-center d-flex flex-column justify-content-center">
+      <h2 className="mb-3">Page Not Found</h2>
+      <Link to="/home" className="text-black text-decoration-none">
+        Return to Home
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
